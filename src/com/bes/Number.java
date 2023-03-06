d2e51d8cc0213fe9fac5e8d886839b89c4f46598
3vgen <56633537+zhen9Bes@users.noreply.github.com>
2023-03-07 00:32:00 +0700

    Add abstract class of Number

---
+++ src/com/bes/Number.java
@@ -0,0 +1,13 @@
+package com.bes;
+
+abstract class Number {
+
+    public abstract Number add(Number number) throws Exception;
+
+    public abstract Number sub(Number number) throws Exception;
+
+    public abstract Number multi(Number number) throws Exception;
+
+    public abstract Number div(Number number) throws Exception;
+
+}
