ba112e19c0b28aa955e387718aca18cfd6de51e9
3vgen <56633537+zhen9Bes@users.noreply.github.com>
2023-03-06 20:41:40 +0700

    Add logic of main function

--- src/com/bes/Main.java
+++ src/com/bes/Main.java
@@ -1,8 +1,21 @@
 package com.bes;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            Scanner scaner = new Scanner(System.in);
+            String userInput = scaner.nextLine();
+            String result = calc(userInput);
+            System.out.println(result);
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+    private static String calc(String input){
+        //TODO: logic
+        return null;
     }
 }
