3bf8706605962b056de0914addc62c776a8d6aff
3vgen <56633537+zhen9Bes@users.noreply.github.com>
2023-03-07 15:39:54 +0700

    Add a selection of the generated class

--- src/com/bes/StringParser.java
+++ src/com/bes/StringParser.java
@@ -6,9 +6,9 @@ class StringParser {
     Number[] numbers;
     Operation operation;
 
-    public StringParser(String str) throws Exception {
-        input = str;
-        words = getWords(str);
+    public StringParser(String input) throws Exception {
+        this.input = input;
+        words = getWords(input);
         operation = getOperation();
         numbers = getNumbers();
     }
@@ -16,22 +16,30 @@ class StringParser {
     private String[] getWords(String str) throws Exception {
         String[] result = str.split(" ");
         if(result.length != 3){
-            throw new Exception("Incorrect input. Invalid number of arguments.");
+            throw new Exception("Incorrect input:" + str + ". Invalid number of operands.");
         }
         return result;
     }
 
     private Number[] getNumbers() throws Exception {
-        //TODO Add notation selection!
         Number[] result = new Number[2];
-        result[0] = new RomanNumber(words[0]);
-        result[1] = new RomanNumber(words[2]);
+        String leftNumber = words[0];
+        String rightNumber = words[2];
+        if(RomanNumber.validate(leftNumber) && RomanNumber.validate(rightNumber)){
+            result[0] = new RomanNumber(leftNumber);
+            result[1] = new RomanNumber(rightNumber);
+        }else if(ArabicNumber.validate(leftNumber) && ArabicNumber.validate(rightNumber)){
+            result[0] = new ArabicNumber(leftNumber);
+            result[1] = new ArabicNumber(rightNumber);
+        }else{
+            throw new Exception("Incorrect input: " + leftNumber + " " + rightNumber +". Different number systems.");
+        }
         return result;
     }
 
     private Operation getOperation() throws Exception {
         if(words[1].length() != 1){
-            throw new Exception("Incorrect input. Unknown operation.");
+            throw new Exception("Incorrect input: " + words[1] + ". Unknown operation.");
         }
         return switch (words[1].charAt(0)) {
                 case '+' -> Operation.ADD;
