aaa2bb7f099908e53e13368e6a1d0a889e2d7713
3vgen <56633537+zhen9Bes@users.noreply.github.com>
2023-03-07 15:40:30 +0700

    Clean Number class

--- src/com/bes/Number.java
+++ src/com/bes/Number.java
@@ -2,12 +2,11 @@ package com.bes;
 
 abstract class Number {
 
-    public abstract Number add(Number number) throws Exception;
+    public abstract Number add(Number number);
 
-    public abstract Number sub(Number number) throws Exception;
+    public abstract Number sub(Number number);
 
-    public abstract Number multi(Number number) throws Exception;
+    public abstract Number multi(Number number);
 
-    public abstract Number div(Number number) throws Exception;
-
+    public abstract Number div(Number number);
 }
